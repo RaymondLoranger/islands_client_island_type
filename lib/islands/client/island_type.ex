3aead5f93c98752f4bc5238b54f368465c70f989
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-06 17:51:17 -0400

    0.1.0

---
+++ lib/islands/client/island_type.ex
@@ -0,0 +1,24 @@
+# ┌─────────────────────────────────────────────────────────────────┐
+# │ Inspired by the course "Elixir for Programmers" by Dave Thomas. │
+# └─────────────────────────────────────────────────────────────────┘
+defmodule Islands.Client.IslandType do
+  use PersistConfig
+
+  @course_ref Application.get_env(@app, :course_ref)
+
+  @moduledoc """
+  Island types for clients of the _Game of Islands_.
+  \n##### #{@course_ref}
+  """
+
+  alias Islands.Island
+
+  @spec new(String.codepoint()) :: Island.type() | {:error, atom}
+  def new(island_type_code)
+  def new("a"), do: :atoll
+  def new("d"), do: :dot
+  def new("l"), do: :l_shape
+  def new("s"), do: :s_shape
+  def new("q"), do: :square
+  def new(_), do: {:error, :invalid_island_type_code}
+end
---
+++ test/islands/client/island_type_test.exs
@@ -0,0 +1,11 @@
+defmodule Islands.Client.IslandTypeTest do
+  use ExUnit.Case, async: true
+
+  alias Islands.Client.IslandType
+
+  doctest IslandType
+
+  test "the truth" do
+    assert 1 + 2 == 3
+  end
+end
