a0da125c196cf8bf53fbd43e7e33db5ac654c956
RaymondLoranger <Raymond.Loranger@bell.net>
2025-08-28 17:47:11 -0400

    0.1.25

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.IslandType.MixProject do
   def project do
     [
       app: :islands_client_island_type,
-      version: "0.1.24",
+      version: "0.1.25",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client Island Type",
